5109389a015504cf64946fa96fbb17e317978779
orestch <o.chuguivets@gmail.com>
2022-11-05 13:10:27 +0200

    Completed Section 1: Getting started

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
 import { name$, storeDataOnServer, storeDataOnServerError } from './external';
 
-name$.subscribe();
+name$.subscribe((value) => console.log(value));
+
+storeDataOnServer('Some value').subscribe({
+  next: (value) => console.log(value),
+});
+
+storeDataOnServerError('Some value').subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log('Error when saving: ', err.message),
+});
